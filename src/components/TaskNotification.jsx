3f51ba9de7ff276a961bc3d7f28f88760b4d6aac
Diggi19 <digveshparab123@gmail.com>
2023-07-16 22:44:10 +0530

    js push to  react-push-notification

--- src/components/TaskNotification.jsx
+++ src/components/TaskNotification.jsx
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import React, { useEffect } from 'react';
+import addNotification from 'react-push-notification';
 
 const TaskNotification = ({ data,settaskList,taskList }) => {
   useEffect(() => {
@@ -18,14 +19,17 @@ const TaskNotification = ({ data,settaskList,taskList }) => {
     const sendNotification = () => {
       // Add your notification logic here
       // This could be an alert, a toast, or a push notification, depending on your application
-      new Notification("Welcome to buddy chair", {
-            icon: "https://cdn.pixabay.com/photo/2017/05/15/21/58/drug-icon-2316244_960_720.png",
-            body: data.reminderMsg
+        addNotification({
+          title: 'Warning',
+          subtitle: 'This is a subtitle',
+          message: 'This is a very long message',
+          theme: 'darkblue',
+          native: true, // when using native, your OS will handle theming.
+          duration:30000
+          
         })
+        
       handleDeleteTask(data.reminderMsg)
-
-      console.log(`It's time for the task: ${data.reminderMsg}`);
-        console.log(taskList)
       clearInterval(intervalId);
     };
 
